bb4fd703b5b0153876cd90c977bf28249e4ebc32
Korobkin Alexander <korobkin.alex2000@yandex.ru>
2020-05-04 19:30:19 +0300

    implement captca

--- src/tag/AddUser.java
+++ src/tag/AddUser.java
@@ -10,7 +10,6 @@ import javax.servlet.jsp.tagext.SimpleTagSupport;
 import java.io.IOException;
 
 public class AddUser extends SimpleTagSupport {
-
     private User user;
 
     public void setUser(User user) {
@@ -19,17 +18,20 @@ public class AddUser extends SimpleTagSupport {
 
     public void doTag() throws JspException, IOException {
         String errorMessage = null;
-        UserList userList = (UserList)
-                getJspContext().getAttribute("users", PageContext.APPLICATION_SCOPE);
-
-        if (user.getLogin() == null || user.getLogin().equals("")) {
+        UserList userList = (UserList)getJspContext().getAttribute("users", PageContext.APPLICATION_SCOPE);
+        if (user.getLogin()==null || user.getLogin().equals("")) {
             errorMessage = "Логин не может быть пустым!";
         } else {
-            if (user.getName() == null || user.getName().equals("")) {
-                errorMessage = "Имя пользователя не может быть пустым!";
+            if (user.getName()==null || user.getName().equals("")) {
+                errorMessage = "Имя пользователя не может‚ быть пустым!";
+            } else {
+                if (user.getName()!=null && !user.getCaptcha().equals(
+                        getJspContext().getAttribute("captcha",PageContext.SESSION_SCOPE))) {
+                    errorMessage = "Не верно введена captcha";
+                }
             }
         }
-        if (errorMessage == null) {
+        if (errorMessage==null) {
             try {
                 userList.addUser(user);
                 UserListHelper.saveUserList(userList);
